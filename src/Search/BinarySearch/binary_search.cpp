e565f844b5bf387052f9168bc94d8c160663c15b
fengjiran <fengjiran@foxmail.com>
2018-11-20 20:02:54 +0800

    binary search

--- src/Search/BinarySearch/binary_search.cpp
+++ src/Search/BinarySearch/binary_search.cpp
@@ -1,4 +1,23 @@
 //
 // Created by Richard on 2018/11/20.
 //
+#include <iostream>
 
+int binarySearch(const int array[], int len, int key) {
+    if (array == nullptr || len <= 0)
+        return -1;
+
+    int l = 0;
+    int h = len - 1;
+    int m;
+    while (l <= h) {
+        m = l + (h - l) / 2;
+        if (key < array[m])
+            h = m - 1;
+        else if (key > array[m])
+            l = m + 1;
+        else
+            return m;
+    }
+    return -1;
+}
