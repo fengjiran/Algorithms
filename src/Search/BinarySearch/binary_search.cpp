0c5166ae001a888564ffafb460bab83a1f9807f4
fengjiran <fengjiran@foxmail.com>
2018-11-20 20:08:20 +0800

    binary search first and last

--- src/Search/BinarySearch/binary_search.cpp
+++ src/Search/BinarySearch/binary_search.cpp
@@ -21,3 +21,51 @@ int binarySearch(const int array[], int len, int key) {
     }
     return -1;
 }
+
+int binarySearchFirst(const int array[], int len, int key) {
+    if (array == nullptr || len <= 0)
+        return -1;
+
+    int l = 0;
+    int h = len - 1;
+    int m;
+
+    while (l <= h) {
+        m = l + (h - l) / 2;
+        if (key < array[m])
+            h = m - 1;
+        else if (key > array[m])
+            l = m + 1;
+        else {
+            while (m >= 0 && key == array[m])
+                m--;
+            m++;
+            return m;
+        }
+    }
+    return -1;
+}
+
+int binarySearchLast(const int array[], int len, int key) {
+    if (array == nullptr || len <= 0)
+        return -1;
+
+    int l = 0;
+    int h = len - 1;
+    int m;
+
+    while (l <= h) {
+        m = l + (h - l) / 2;
+        if (key < array[m])
+            h = m - 1;
+        else if (key > array[m])
+            l = m + 1;
+        else {
+            while (m <= len - 1 && key == array[m])
+                m++;
+            m--;
+            return m;
+        }
+    }
+    return -1;
+}
