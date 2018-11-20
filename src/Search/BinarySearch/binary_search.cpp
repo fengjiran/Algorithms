af7578384b77d1afb31c1708a212b8b696c17b98
fengjiran <fengjiran@foxmail.com>
2018-11-20 20:11:29 +0800

    add comments

--- src/Search/BinarySearch/binary_search.cpp
+++ src/Search/BinarySearch/binary_search.cpp
@@ -1,8 +1,10 @@
 //
 // Created by Richard on 2018/11/20.
 //
-#include <iostream>
 
+// 假设数组是递增序列
+// 一般情况下的二分查找，经典的二分查找
+// 没有考虑数组中具有多个重复的key的情况
 int binarySearch(const int array[], int len, int key) {
     if (array == nullptr || len <= 0)
         return -1;
@@ -22,7 +24,11 @@ int binarySearch(const int array[], int len, int key) {
     return -1;
 }
 
+
+// 假设数组是非递减序列
+// 寻找重复key中第一个出现的位置
 int binarySearchFirst(const int array[], int len, int key) {
+    // 判断数组是否为空
     if (array == nullptr || len <= 0)
         return -1;
 
@@ -46,7 +52,11 @@ int binarySearchFirst(const int array[], int len, int key) {
     return -1;
 }
 
+
+// 假设数组是非递减序列
+// 寻找重复key中最后一个出现的位置
 int binarySearchLast(const int array[], int len, int key) {
+    // 判断数组是否为空
     if (array == nullptr || len <= 0)
         return -1;
 
