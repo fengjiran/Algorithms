89feb942053c7b195a27f39644ca2d1a205a6a34
fengjiran <fengjiran@foxmail.com>
2018-11-21 09:30:56 +0800

    add test file using catch

--- header/algorithms.h
+++ header/algorithms.h
@@ -5,4 +5,10 @@
 #ifndef ALGORITHMS_ALGORITHMS_H
 #define ALGORITHMS_ALGORITHMS_H
 
+int binarySearch(const int array[], int len, int key);
+
+int binarySearchFirst(const int array[], int len, int key);
+
+int binarySearchLast(const int array[], int len, int key);
+
 #endif //ALGORITHMS_ALGORITHMS_H
