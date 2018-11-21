3d13a4f30695a709773d9976e2d64851ccb4aa0b
fengjiran <fengjiran@foxmail.com>
2018-11-21 09:36:36 +0800

    add test case for binary search

--- test/test_binary_search.cpp
+++ test/test_binary_search.cpp
@@ -2,3 +2,14 @@
 // Created by Richard on 2018/11/21.
 //
 
+#include "../header/catch.hpp"
+#include "../header/algorithms.h"
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+int a[] = {2, 3, 3, 3, 3, 4, 5};
+int len = sizeof(a);
+
+TEST_CASE("Binary Search is testing", "[BinarySearch]") {
+    REQUIRE(binarySearch(a, len, 3) == 3);
+}
