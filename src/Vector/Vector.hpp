7597d7bb47fc9835b7d68d66fcdbb8c0ce4f3337
fengjiran <fengjiran@foxmail.com>
2018-11-23 13:08:07 +0800

    add Vector class template

--- src/Vector/Vector.hpp
+++ src/Vector/Vector.hpp
@@ -5,4 +5,23 @@
 #ifndef ALGORITHMS_VECTOR_HPP
 #define ALGORITHMS_VECTOR_HPP
 
+typedef int Rank;
+const int DEFAULT_CAPACITY = 3;
+
+template<typename T>
+class Vector {
+protected:
+    Rank _size;  //
+    int _capacity;  //
+    T *_elem;  //
+    void copyFrom(const T *A, Rank lo, Rank hi);  //
+    void expand();  //
+
+    void shrink();  //
+
+    bool bubble(Rank lo, Rank hi);  //
+
+    void bubbleSort(Rank lo, Rank hi);  //
+};
+
 #endif //ALGORITHMS_VECTOR_HPP
